1999af8a0930763f170833141f750b7c075db20c
Alin Florin <alin@qoffice.ro>
2021-12-31 03:40:34 +0200

    g

--- src/app/Conversation.tsx
+++ src/app/Conversation.tsx
@@ -1,13 +1,13 @@
 import {
-  DocumentCard,
-  DocumentCardActivity,
-  DocumentCardDetails,
-  DocumentCardTitle,
-  DocumentCardType,
-  IDocumentCardActivityPerson,
+  Checkbox,
+  Persona,
+  PersonaPresence,
+  PrimaryButton,
   Stack,
+  Text,
+  TextField,
 } from "@fluentui/react";
-import { useMemo } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useDocumentData } from "react-firebase-hooks/firestore";
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
@@ -26,43 +26,100 @@ const getInitials = (n: string | null) => {
 };
 
 export const Conversation = () => {
+  const [key, setKey] = useState<string | undefined>();
   const params = useParams();
+
+  const [tempKey, setTempKey] = useState<string | undefined>();
+  const [persist, setPersist] = useState<boolean>(false);
+
+  useEffect(() => {
+    const lsKey = localStorage.getItem("key_" + params.friendEmail);
+    if (lsKey != null) {
+      setKey(lsKey);
+    }
+  }, [params.friendEmail, setKey]);
+
   const [t] = useTranslation();
   const [friendProfile, friendProfileLoading] = useDocumentData<Profile>(
     getDocumentRef(`profiles/${params.friendEmail}`)
   );
 
-  const person: IDocumentCardActivityPerson | undefined = useMemo(() => {
-    if (friendProfile == null) {
-      return undefined;
+  const saveKey = useCallback(() => {
+    setKey(tempKey!);
+    if (persist) {
+      localStorage.setItem("key_" + params.friendEmail, tempKey!);
     }
-    return {
-      name: friendProfile.displayName,
-      profileImageSrc: friendProfile.photo,
-      initials: getInitials(friendProfile.displayName!),
-    } as IDocumentCardActivityPerson;
-  }, [friendProfile]);
+  }, [setKey, tempKey, persist, params.friendEmail]);
 
   return (
     <>
       {!friendProfileLoading && friendProfile && (
         <Stack horizontal={false} verticalFill={true}>
-          <DocumentCardDetails>
-            {person && (
-              <DocumentCardActivity
-                activity={
-                  isOnline(friendProfile!.lastSeen)
-                    ? t("ui.conversation.online")
-                    : friendProfile!.lastSeen == null
-                    ? t("ui.conversation.offline")
-                    : t("ui.conversation.lastSeenAt") +
-                      " " +
-                      friendProfile!.lastSeen!.toDate().toLocaleString()
-                }
-                people={[person]}
-              />
-            )}
-          </DocumentCardDetails>
+          <Stack
+            tokens={{
+              childrenGap: "1rem",
+            }}
+            horizontal={true}
+            styles={{ root: { width: "100%" } }}
+          >
+            <Persona
+              imageUrl={friendProfile.photo || undefined}
+              imageInitials={getInitials(friendProfile.displayName || null)}
+              text={friendProfile.displayName || undefined}
+              key={isOnline(friendProfile!.lastSeen).toString()}
+              presence={
+                isOnline(friendProfile!.lastSeen)
+                  ? PersonaPresence.online
+                  : PersonaPresence.offline
+              }
+              hidePersonaDetails={true}
+            />
+            <Stack verticalAlign="center" horizontal={false} grow={true}>
+              <Text variant="xLarge">{friendProfile.displayName}</Text>
+              <Text variant="small">
+                {isOnline(friendProfile!.lastSeen)
+                  ? t("ui.conversation.online")
+                  : friendProfile!.lastSeen == null
+                  ? t("ui.conversation.offline")
+                  : t("ui.conversation.lastSeenAt") +
+                    " " +
+                    friendProfile!.lastSeen!.toDate().toLocaleString()}
+              </Text>
+            </Stack>
+          </Stack>
+
+          {!key && (
+            <Stack
+              horizontal={false}
+              verticalFill={true}
+              styles={{ root: { minHeight: 0, overflow: "auto" } }}
+            >
+              <Stack
+                horizontalAlign="center"
+                horizontal={false}
+                tokens={{ childrenGap: "1rem" }}
+                styles={{ root: { margin: "auto" } }}
+              >
+                <TextField
+                  iconProps={{ iconName: "PasswordField" }}
+                  type="password"
+                  value={tempKey}
+                  onChange={(e) => setTempKey((e.target as any).value)}
+                  label={t("ui.conversation.key")}
+                />
+                <Checkbox
+                  checked={persist}
+                  onChange={(e) => setPersist((e!.target as any).checked)}
+                  label={t("ui.conversation.persist")}
+                />
+                <PrimaryButton
+                  disabled={!tempKey}
+                  onClick={saveKey}
+                  text={t("ui.conversation.go")}
+                />
+              </Stack>
+            </Stack>
+          )}
         </Stack>
       )}
     </>
