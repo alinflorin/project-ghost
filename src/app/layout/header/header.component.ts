fe9110bec5d0b87e86cfa66cb79dfd2887012bd3
Alin Florin <alin@qoffice.ro>
2022-01-02 02:24:30 +0200

    t

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,6 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Auth, User, user, signOut } from '@angular/fire/auth';
 import { MatDrawer } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { from, Subscription } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -8,14 +11,25 @@ import { environment } from 'src/environments/environment';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   @Input('isMobile') isMobile = false;
   @Input('drawer') drawer: MatDrawer | undefined;
   allLanguages = environment.availableLanguages;
+  private _destroy: Subscription[] = [];
+  user: User | null = null;
 
-  constructor(public translateService: TranslateService) { }
+  constructor(public translateService: TranslateService, private auth: Auth, private router: Router) { }
 
   ngOnInit(): void {
+    this._destroy.push(
+      user(this.auth).subscribe(u => {
+        this.user = u;
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this._destroy.forEach(x => x.unsubscribe());
   }
 
   changeLanguage(code: string) {
@@ -24,5 +38,13 @@ export class HeaderComponent implements OnInit {
 
   toggleDrawer() {
     this.drawer?.toggle();
+  }
+
+  logout() {
+    from(signOut(this.auth)).subscribe({
+      next: () => {
+        this.router.navigate(['login']);
+      }
+    });
   }
 }
