0a29bab114846b5f391d46f559e287b8439be5df
Alin Florin <alin@qoffice.ro>
2022-01-03 15:11:52 +0200

    g

--- src/app/workers/rsa.worker.ts
+++ src/app/workers/rsa.worker.ts
@@ -1,7 +1,46 @@
 /// <reference lib="webworker" />
 
+importScripts(`/assets/js/cryptico.js`);
+declare var cryptico: any;
+const api = cryptico.cryptico;
+(function (c) {
+  var parametersBigint = ["n", "d", "p", "q", "dmp1", "dmq1", "coeff"];
+
+  c.privateKeyString = function (rsakey: any) {
+    var keyObj: any = {};
+    parametersBigint.forEach(function (parameter) {
+      keyObj[parameter] = c.b16to64(rsakey[parameter].toString(16));
+    });
+    return JSON.stringify(keyObj);
+  };
+  c.privateKeyFromString = function (string: string) {
+    var keyObj = JSON.parse(string);
+    var rsa: any = new cryptico.RSAKey();
+    parametersBigint.forEach(function (parameter) {
+      rsa[parameter] = cryptico.parseBigInt(c.b64to16(keyObj[parameter].split("|")[0]), 16);
+    });
+    rsa.e = parseInt("03", 16);
+    return rsa;
+  };
+})(api);
 
 addEventListener('message', ({ data }) => {
-  const response = `worker response to ${data}`;
-  postMessage(response);
+  let r = null;
+  console.log(data);
+  switch (data.type) {
+    case 0:
+      const rsaKeyBits = api.generateRSAKey(data.data.passphrase, 1024);
+      r = {
+        type: 1,
+        data: {
+          rsaKey: api.privateKeyString(rsaKeyBits),
+          publicKey: api.publicKeyString(rsaKeyBits)
+        }
+      };
+      break;
+    default:
+      throw 'Unknown message';
+  }
+  console.log(r);
+  postMessage(r);
 });
